75dbd3ec3e66a79a08b7b2c2bf5415d6b5609264
James Martinez <jnez.81@gmail.com>
2021-01-27 16:00:56 -0700

    Initial Commit

---
+++ Sportsbar Locator/Sportsbar_LocatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sportsbar_LocatorApp.swift
+//  Sportsbar Locator
+//
+//  Created by James Martinez on 1/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Sportsbar_LocatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
